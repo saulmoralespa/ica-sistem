f11bb655a9191736aa756494b420bc9769bc9806
Saul Morales <info@saulmoraespa.com>
2018-10-28 14:05:30 -0500

    Procfile for Heroku

--- heroku.sh
+++ heroku.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 heroku config:set DB_CONNECTION=pgsql
-heroku config:set DB_HOST=ec2-174-129-32-37.compute-1.amazonaws.com
+heroku config:set DB_HOST=ec2-54-83-49-109.compute-1.amazonaws.com
 heroku config:set DB_PORT=5432
-heroku config:set DB_DATABASE=d9e6k8hehfqlu2
-heroku config:set DB_USERNAME=vvnpgmbnkttkry
-heroku config:set DB_PASSWORD=1f067e2f34fef51159764491844291677d0070aa09de7252d609c28f9c57d3f1
+heroku config:set DB_DATABASE=d62jo1jcj1j6lv
+heroku config:set DB_USERNAME=dupduzqtnyycus
+heroku config:set DB_PASSWORD=0cd3fa88f36c0920a61eb0736383b4081193f9c459342b0d9e287cbea67d2361
 
 heroku config:set APP_NAME=ICA
-heroku config:set APP_URL=http://ica-system.herokuapp.com
+heroku config:set APP_URL=http://ica-sistem.herokuapp.com
 
 heroku config:set MAIL_USERNAME=6562f3bf34856d
 heroku config:set MAIL_PASSWORD=20855b2660ab27
