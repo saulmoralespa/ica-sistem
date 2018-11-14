fe975fe911804f4ba38bf13c1a893f04d479544d
Saul Morales <info@saulmoraespa.com>
2018-11-14 12:11:42 -0500

    add logo header init

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,73 +1,69 @@
-<header id="m_header" class="m-grid__item    m-header " m-minimize="minimize" m-minimize-mobile="minimize" m-minimize-offset="200" m-minimize-mobile-offset="200">
-    <div class="m-container m-container--fluid m-container--full-height">
-        <div class="m-stack m-stack--ver m-stack--desktop  m-header__wrapper">
+<header id="m_header" class="m-grid__item m-header " m-minimize="minimize" m-minimize-mobile="minimize" m-minimize-offset="10" m-minimize-mobile-offset="10">
+    <div class="m-header__top">
+        <div class="m-container m-container--fluid m-container--full-height m-page__container">
+            <div class="m-stack m-stack--ver m-stack--desktop">
 
-            <!-- BEGIN: Brand -->
-            <div class="m-stack__item m-brand m-brand--mobile">
-                <div class="m-stack m-stack--ver m-stack--general">
-                    <div class="m-stack__item m-stack__item--middle m-brand__logo">
-                    </div>
-                    <div class="m-stack__item m-stack__item--middle m-brand__tools">
-
-                        <!-- BEGIN: Responsive Header Menu Toggler -->
-                        <a id="m_aside_header_menu_mobile_toggle" href="javascript:;" class="m-brand__icon m-brand__toggler">
-                            <span></span>
-                        </a>
-
-                        <!-- END -->
-
-                        <!-- BEGIN: Topbar Toggler -->
-                        <a id="m_aside_header_topbar_mobile_toggle" href="javascript:;" class="m-brand__icon">
-                            <i class="flaticon-more"></i>
-                        </a>
-
-                        <!-- BEGIN: Topbar Toggler -->
+                <!-- begin::Brand -->
+                <div class="m-stack__item m-brand m-stack__item--left">
+                    <div class="m-stack m-stack--ver m-stack--general m-stack--inline">
+                        <div class="m-stack__item m-stack__item--middle m-brand__logo">
+                            <a href="{{ route('home') }}" class="m-brand__logo-wrapper">
+                                {{--<img alt="" src="{{ asset('images/logo.jpg') }}" class="m-brand__logo-desktop">--}}
+                                {{--<img alt="" src="assets/demo/demo10/media/img/logo/logo_mini.png" class="m-brand__logo-mobile" />--}}
+                            </a>
+                        </div>
+                        <div class="m-stack__item m-stack__item--middle m-brand__tools">
+                            <!-- begin::Responsive Header Menu Toggler-->
+                            <a id="m_aside_header_menu_mobile_toggle" href="javascript:;" class="m-brand__icon m-brand__toggler m--visible-tablet-and-mobile-inline-block">
+                                <span></span>
+                            </a>
+                            <!-- end::Responsive Header Menu Toggler-->
+                            <!-- begin::Topbar Toggler-->
+                            {{--<a id="m_aside_header_topbar_mobile_toggle" href="javascript:;" class="m-brand__icon m--visible-tablet-and-mobile-inline-block">
+                                <i class="flaticon-more"></i>
+                            </a>--}}
+                            <!--end::Topbar Toggler-->
+                        </div>
                     </div>
                 </div>
-            </div>
-
-            <!-- END: Brand -->
-            <div class="m-stack__item m-stack__item--middle m-stack__item--left m-header-head" id="m_header_nav">
-                <div class="m-stack m-stack--ver m-stack--desktop">
-                    <div class="m-stack__item m-stack__item--fluid">
-
-                        <!-- BEGIN: Horizontal Menu -->
-                        <button class="m-aside-header-menu-mobile-close  m-aside-header-menu-mobile-close--skin-dark " id="m_aside_header_menu_mobile_close_btn">
-                            <i class="la la-close"></i>
-                        </button>
-                        <div id="m_header_menu" class="m-header-menu m-aside-header-menu-mobile m-aside-header-menu-mobile--offcanvas  m-header-menu--skin-light m-header-menu--submenu-skin-light m-aside-header-menu-mobile--skin-dark m-aside-header-menu-mobile--submenu-skin-dark ">
-                            <ul class="m-menu__nav  m-menu__nav--submenu-arrow ">
-                                @include(' navigations.students')
-                                @include(' navigations.payments')
-                                @include(' navigations.costs')
-                                @include(' navigations.reports')
-                                @include(' navigations.users')
+                <!-- end::Brand -->
+                <!-- begin::Topbar -->
+                <div class="m-stack__item m-stack__item--right m-header-head" id="m_header_nav">
+                    <div id="m_header_topbar" class="m-topbar  m-stack m-stack--ver m-stack--general">
+                        <div class="m-stack__item m-topbar__nav-wrapper">
+                            <ul class="m-topbar__nav m-nav m-nav--inline">
+                                {{--@include('dashboard.alarm_notifications')
+                                @include('dashboard.quick_actions')--}}
+                                @include('dashboard.carduser')
                             </ul>
                         </div>
-
-                        <!-- END: Horizontal Menu -->
                     </div>
                 </div>
+                <!-- end::Topbar -->
             </div>
-            <div class="m-stack__item m-stack__item--middle m-stack__item--center">
+        </div>
+    </div>
+    <div class="m-header__bottom">
+        <div class="m-container m-container--fluid m-container--full-height m-page__container">
+            <div class="m-stack m-stack--ver m-stack--desktop">
 
-                <!-- BEGIN: Brand -->
-                {{--<a href="index.html" class="m-brand m-brand--desktop">
-                    <img alt="" src="assets/demo/demo9/media/img/logo/logo.png" />
-                </a>--}}
-
-                <!-- END: Brand -->
-            </div>
-            <div class="m-stack__item m-stack__item--right">
-
-                <!-- BEGIN: Topbar -->
-                <div id="m_header_topbar" class="m-topbar  m-stack m-stack--ver m-stack--general">
-                    <div class="m-stack__item m-topbar__nav-wrapper">
-                        <ul class="m-topbar__nav m-nav m-nav--inline">
-                            @include('dashboard.carduser')
+                <!-- begin::Horizontal Menu -->
+                <div class="m-stack__item m-stack__item--fluid m-header-menu-wrapper">
+                    <button class="m-aside-header-menu-mobile-close  m-aside-header-menu-mobile-close--skin-light " id="m_aside_header_menu_mobile_close_btn">
+                        <i class="la la-close"></i>
+                    </button>
+                    <div id="m_header_menu" class="m-header-menu m-aside-header-menu-mobile m-aside-header-menu-mobile--offcanvas  m-header-menu--skin-dark m-header-menu--submenu-skin-light m-aside-header-menu-mobile--skin-light m-aside-header-menu-mobile--submenu-skin-light ">
+                        <ul class="m-menu__nav  m-menu__nav--submenu-arrow ">
+                            @include(' navigations.students')
+                            @include(' navigations.payments')
+                            @include(' navigations.costs')
+                            @include(' navigations.reports')
+                            @include(' navigations.users')
                         </ul>
                     </div>
                 </div>
+
+                <!-- end::Horizontal Menu -->
             </div>
         </div>
     </div>
